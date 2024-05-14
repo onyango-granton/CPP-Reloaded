e847649efa5bbe9d9fd2bbac40e7756fe1b65e5a
gonyango <grantononyango@gmail.com>
2024-05-14 17:33:50 +0300

    Fixed bug in count

--- alphabetOcc.cpp
+++ alphabetOcc.cpp
@@ -8,20 +8,30 @@ void AlphaInString(){
     map<char, int> mp;
     string s1 {"Hello world"};
     for (char ch{'a'}; ch <= 'z';ch++){
-        int count;
+        int count = 0;
         for (int i{0}; i < s1.length();i++){
             if (ch == s1[i]){
                 count++;
             }
         }
+        // cout << count;
         mp[ch] = count; 
     }
+
+    cout << mp.begin()->second;
+
     map<char, int>::iterator it = mp.begin();
 
-    while (it !=)
+    while (it != mp.end()){
+        cout << "Key:" << it->first
+        <<", Value: "<< it->second<< endl;
+        it++;
+
+    }
 
 }
 
 int main(){
+    AlphaInString();
     return 0;
 }
