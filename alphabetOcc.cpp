2b54ffbdc6d51a86b7aca4cd3c657e2f31af0cf3
gonyango <grantononyango@gmail.com>
2024-05-14 17:45:46 +0300

    Fixed some bugs

--- alphabetOcc.cpp
+++ alphabetOcc.cpp
@@ -18,14 +18,19 @@ void AlphaInString(){
         mp[ch] = count; 
     }
 
-    cout << mp.begin()->second;
+    // cout << mp.begin()->second;
 
     map<char, int>::iterator it = mp.begin();
 
     while (it != mp.end()){
-        cout << "Key:" << it->first
-        <<", Value: "<< it->second<< endl;
+        if (it->second > 0){
+            cout << it -> first <<" appeared "
+            <<it -> second<<" times"<<endl;
+        }
         it++;
+        // cout << "Key:" << it->first
+        // <<", Value: "<< it->second<< endl;
+        // it++;
 
     }
 
