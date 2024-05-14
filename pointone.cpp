fb74d5e5845afcef961dce8dce31b570b61d1a89
gonyango <grantononyango@gmail.com>
2024-05-14 18:15:45 +0300

    Pointer function to change value at a memory address

--- pointone.cpp
+++ pointone.cpp
@@ -2,13 +2,16 @@
 
 using namespace std;
 
+// PointOne takes a pointer to an integer
+// Within the memory address it changes the data stored to 1
 int PointOne(int* n){
+    // Dereferencing
     *n = 1;
     return *n;
 }
 
 int main() {
     int num {1};
-    cout << PointOne(num);
+    cout << PointOne(&num)<<endl;
     return 0;
 }
