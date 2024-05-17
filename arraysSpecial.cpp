889fe7f0b9833135e54129740e0196d5a8786540
gonyango <grantononyango@gmail.com>
2024-05-17 09:32:40 +0300

    Covering arrays in cpp

---
+++ arraysSpecial.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string cars[5] = {"Volvo","Merce","Porsche","Bently"};
+    for (string i: cars){
+        cout << i<< endl;
+    }
+
+}
